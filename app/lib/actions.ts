c9e745448324920db2c6709a5353bd23193eafbb
Lucas Legrand <legrand.lucas0@gmail.com>
2024-04-21 20:44:54 +0200

    edit invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,8 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -34,4 +36,28 @@ export async function createInvoice(formData: FormData) {
 revalidatePath('/dashboard/invoices');
 redirect('/dashboard/invoices');
 
+}
+/* Extracting the data from formData.
+Validating the types with Zod.
+Converting the amount to cents.
+Passing the variables to your SQL query.
+Calling revalidatePath to clear the client cache and make a new server request.
+Calling redirect to redirect the user to the invoice's page. */
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
